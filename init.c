aefdc8855f06f39a552412b3cd6f17fd4dcbb735
ExtReMLapin <ExtReMLapin@users.noreply.github.com>
2016-07-25 16:32:58 +0200

    Update init.c

--- init.c
+++ init.c
@@ -10,11 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/fdf.h"
+#include "include/wtd.h"
 #include "minilibx_macos/mlx.h"
 #include <stdio.h>
 
-unsigned long		creatergb(int r, int g, int b)
+unsigned long				creatergb(int r, int g, int b)
 {
 	return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
 }
@@ -23,14 +23,14 @@ static int					envint(t_env *env)
 {
 	env->w = 1600;
 	env->h = 900;
-	env->posX = 22; //x and y start position
-	env->posY = 12;
-	env->dirX = -1;
-	env->dirY = 0; //initial direction vector
-	env->planeX = 0;
-	env->planeY = 0.66; //the 2d raycaster version of camera plane				
-	env->time = clock(); //time of current frame
-	env->oldtime = clock(); //time of previous frame
+	env->posx = 22;
+	env->posy = 12;
+	env->dirx = -1;
+	env->diry = 0;
+	env->planex = 0;
+	env->planey = 0.66;
+	env->time = clock();
+	env->oldtime = clock();
 	env->frametime = 1;
 	env->up = 0;
 	env->down = 0;
@@ -39,12 +39,29 @@ static int					envint(t_env *env)
 	return (1);
 }
 
+
+void verlineex(t_env *env, int x, int col)
+{
+	int i;
+	int y1;
+	int y2;
+
+	y1 = 20;
+	y2 = 50;
+	i = 0;
+	while (i < (y2 - y1))
+	{
+		fastmlx_pixel_put(env, x, y1+i, col);
+		i++;
+	}
+}
+
 static void					checkmlx(t_env *env)
 {
 	env->mlx = mlx_init();
 	if (!(env->mlx))
 		error("FAILED TO INIT MLX/MAYBE LINKING TO X11 SERVER");
-	env->win = mlx_new_window(env->mlx, env->w, env->h, "nVishit FLEX DEMO BUILD #4351 [JUL-2016]");
+	env->win = mlx_new_window(env->mlx, env->w, env->h, "Wolf3D");
 	if (!(env->win))
 		error("FAILED TO CREATE X11 WINDOW");
 	env->img = mlx_new_image(env->mlx, env->w, env->h);
@@ -56,8 +73,8 @@ static void					checkmlx(t_env *env)
 
 static void					initenv(t_env *env)
 {
-		envint(env);
-		checkmlx(env);
+	envint(env);
+	checkmlx(env);
 }
 
 int							main(void)
@@ -66,13 +83,10 @@ int							main(void)
 
 	env = (t_env *)malloc(sizeof(t_env));
 	env->w3d = (t_w3d *)malloc(sizeof(t_w3d));
-
 	initenv(env);
 	mlx_expose_hook(env->win, draw, env);
 	mlx_hook(env->win, 2, (1L << 0), key_press, env);
 	mlx_hook(env->win, 3, (1L << 1), key_release, env);
-	mlx_hook(env->win, 6, 64, mousekey, env);
-	mlx_mouse_hook(env->win, mousebutton, env);
 	mlx_loop_hook(env->mlx, hook_loop, env);
 	mlx_do_key_autorepeatoff(env->mlx);
 	mlx_loop(env->mlx);
