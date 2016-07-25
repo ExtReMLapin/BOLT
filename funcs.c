cbbf881f530c34c12291617e0749d70410ca2c66
ExtReMLapin <ExtReMLapin@users.noreply.github.com>
2016-07-25 16:32:34 +0200

    Update funcs.c

--- funcs.c
+++ funcs.c
@@ -10,8 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/fdf.h"
-#include "minilibx_macos/mlx.h"
+#include "include/wtd.h"
 
 void	ft_putchar(char c)
 {
@@ -47,4 +46,31 @@ void	errornohalt(const char *s)
 	ft_putstr(s);
 	ft_putstr("\033[0m");
 	ft_putchar('\n');
+}
+
+unsigned int		hsv(long double v)
+{
+	t_hsv	hsv;
+
+	hsv.i = floor(v / 60);
+	hsv.f = (v / 60) - hsv.i;
+	hsv.l = v * (1 - v);
+	hsv.m = v * (1 - hsv.f * v);
+	hsv.n = v * (1 - (1 - hsv.f) * v);
+	hsv.l *= 255;
+	hsv.m *= 255;
+	hsv.n *= 255;
+	v *= 255;
+	if (hsv.i == 0)
+		return (((int)v << 16) + ((int)hsv.n << 8) + (int)hsv.l);
+	else if (hsv.i == 1)
+		return (((int)hsv.m << 16) + ((int)v << 8) + (int)hsv.l);
+	else if (hsv.i == 2)
+		return (((int)hsv.l << 16) + ((int)v << 8) + (int)hsv.n);
+	else if (hsv.i == 3)
+		return (((int)hsv.l << 16) + ((int)hsv.m << 8) + (int)v);
+	else if (hsv.i == 4)
+		return (((int)hsv.n << 16) + ((int)hsv.l << 8) + (int)v);
+	else
+		return (((int)v << 16) + ((int)hsv.l << 8) + (int)hsv.m);
 }
