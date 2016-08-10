c80c05aaf74fef8dfa870601220358e65ff5331c
ExtReMLapin <ExtReMLapin@users.noreply.github.com>
2016-08-10 15:22:13 +0200

    Update hook.c

--- hook.c
+++ hook.c
@@ -3,86 +3,138 @@
 /*                                                        :::      ::::::::   */
 /*   hook.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pfichepo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pfichepo <pfichepo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/15 14:27:37 by pfichepo          #+#    #+#             */
-/*   Updated: 2016/03/15 14:27:39 by pfichepo         ###   ########.fr       */
+/*   Updated: 2016/08/10 14:54:34 by pfichepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/fdf.h"
+#include "include/wtd.h"
 #include "minilibx_macos/mlx.h"
+#include <stdio.h>
 
-static void			movething(t_env *env, char c, int dest)
+int worldmap2[MAPWIDTH][MAPHEIGHT] =
 {
-	t_point		*pts;
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 0, 0, 0, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+};
 
-	pts = env->grid;
-	while (pts)
+static int		check_rot(t_env *env)
+{
+	double		rd;
+	double		olddirx;
+	double		oldplanex;
+
+	olddirx = env->dirx;
+	oldplanex = env->planex;
+	rd = env->frametime * 1.0;
+	if (env->right)
 	{
-		if (c == 'y')
-			pts->y_2d += dest * 10;
-		if (c == 'x')
-			pts->x_2d += dest * 10;
-		pts = pts->next;
-	}
-	draw(env);
-}
-
-static void			subcall(t_env *env, int keycode)
-{
-	if (keycode == 126)
-		movething(env, 'y', -1);
-	if (keycode == 125)
-		movething(env, 'y', 1);
-	if (keycode == 123)
-		movething(env, 'x', -1);
-	if (keycode == 124)
-		movething(env, 'x', 1);
-}
-
-static int			randomcolor(void)
-{
-	int rand1;
-	int rand2;
-	int rand3;
-
-	rand1 = rand() % 255 + 50;
-	rand2 = rand() % 255 + 50;
-	rand3 = rand() % 255 + 50;
-	return (creatergb(rand1, rand2, rand3));
-}
-
-int					hookkey(int keycode, t_env *env)
-{
-	subcall(env, keycode);
-	if (keycode == 53)
-	{
-		mlx_destroy_window(env->mlx, env->win);
-		exit(EXIT_SUCCESS);
+		env->dirx = env->dirx * cos(-rd) - env->diry * sin(-rd);
+		env->diry = olddirx * sin(-rd) + env->diry * cos(-rd);
+		env->planex = env->planex * cos(-rd) - env->planey * sin(-rd);
+		env->planey = oldplanex * sin(-rd) + env->planey * cos(-rd);
 		return (1);
 	}
-	if (keycode == 36)
+	if (env->left)
 	{
-		if (env->rendermode == 3)
-			env->rendermode = 2;
-		else
-			env->rendermode = 3;
-		draw(env);
+		env->dirx = env->dirx * cos(rd) - env->diry * sin(rd);
+		env->diry = olddirx * sin(rd) + env->diry * cos(rd);
+		env->planex = env->planex * cos(rd) - env->planey * sin(rd);
+		env->planey = oldplanex * sin(rd) + env->planey * cos(rd);
 		return (1);
-	}
-	if (keycode == 49)
-	{
-		env->c = randomcolor();
-		draw(env);
 	}
 	return (0);
 }
 
-inline int			fastmlx_pixel_put(t_env *env, int x, int y, int color)
+static int		check_move(t_env *env)
+{
+	double	md;
+
+	md = env->frametime * 5.0;
+	if (env->up)
+	{
+		if (worldmap2[(int)(env->posx + env->dirx * md)][(int)(env->posy)] == 0)
+			env->posx += env->dirx * md;
+		if (worldmap2[(int)(env->posx)][(int)(env->posy + env->diry *
+		md)] == 0)
+			env->posy += env->diry * md;
+		return (1);
+	}
+	if (env->down)
+	{
+		if (worldmap2[(int)(env->posx - env->dirx * md)][(int)(env->posy)] == 0)
+			env->posx -= env->dirx * md;
+		if (worldmap2[(int)(env->posx)][(int)(env->posy - env->diry *
+		md)] == 0)
+			env->posy -= env->diry * md;
+		return (1);
+	}
+	return (0);
+}
+
+int			hook_loop(t_env *env)
+{
+	if (env->posx < 0 || env->posy < 0 || env->posx > MAPWIDTH
+		|| env->posy > MAPHEIGHT)
+	{
+		env->posx = 22;
+		env->posy = 12;
+	}
+	if (check_move(env) + check_rot(env) != 0)
+		draw(env);
+	return (1);
+}
+
+int			key_press(int keycode, t_env *env)
+{
+	if (keycode == KEY_RIGHT)
+		env->right = 1;
+	if (keycode == KEY_LEFT)
+		env->left = 1;
+	if (keycode == KEY_UP)
+		env->up = 1;
+	if (keycode == KEY_DOWN)
+		env->down = 1;
+	return (1);
+}
+
+int			key_release(int keycode, t_env *env)
+{
+	if (keycode == KEY_RIGHT)
+		env->right = 0;
+	if (keycode == KEY_LEFT)
+		env->left = 0;
+	if (keycode == KEY_UP)
+		env->up = 0;
+	if (keycode == KEY_DOWN)
+		env->down = 0;
+	return (1);
+}
+
+int			fastmlx_pixel_put(t_env *env, int x, int y, int color)
 {
 	int h;
 	int w;
+	int off;
 
 	h = env->h;
 	w = env->w;
@@ -90,6 +142,7 @@ inline int			fastmlx_pixel_put(t_env *env, int x, int y, int color)
 		return (0);
 	if ((y > h) || (y < 0))
 		return (0);
-	*(unsigned int*)(env->data + (x * 4) + (env->size_line * y)) = mlx_get_color_value(env->mlx, color);
+	off = (x * 4) + (env->size_line * y);
+	*(unsigned int*)(env->data + off) = mlx_get_color_value(env->mlx, color);
 	return (1);
 }
